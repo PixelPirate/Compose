b3107db29a16cb558908343000627afd088be995
pho <pho@starfinanz.de>
2025-12-15 11:17:11 +0100

    Add documentation.

--- Sources/Components/Component System/Query Parts/ChangeFilters.swift
+++ Sources/Components/Component System/Query Parts/ChangeFilters.swift
@@ -1,7 +1,9 @@
+/// Causes a query to only return results when `Component` got added in the previous frame. This does not apply for a spawn with the given component.
 public struct Added<C: Component>: Component {
     public static var componentTag: ComponentTag { C.componentTag }
 }
 
+/// Causes a query to only return results when `Component` was changed in the previous frame.
 public struct Changed<C: Component>: Component {
     public static var componentTag: ComponentTag { C.componentTag }
 }
